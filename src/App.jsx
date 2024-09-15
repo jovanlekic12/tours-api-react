453269fcaa1cde8f1ead84763f36bbe91334cb45
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-15 23:55:50 +0200

    add data and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import Tour from "./tour";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tours, setTours] = useState([]);
+  const [error, setError] = useState(null);
+  const url = "https://www.course-api.com/react-tours-project";
+
+  function handleDeleteTour(id) {
+    setTours((arr) => arr.filter((tour) => tour.id !== id));
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setTours(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
+      <div className="container">
+        <h1>Our Tours</h1>
+        {error && <p>Error: {error.message}</p>}
+        <ul className="tours__list">
+          {tours.map((tour) => {
+            return (
+              <Tour
+                key={tour.id}
+                {...tour}
+                handleDeleteTour={handleDeleteTour}
+              />
+            );
+          })}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/tour.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Tour(props) {
+  const { id, name, info, image, price, handleDeleteTour } = props;
+  return (
+    <li className="list__item">
+      <div className="image__container">
+        <img className="image" src={image} alt="" />
+        <p className="price">${price}</p>
+      </div>
+      <div className="info__container">
+        <h3 className="tour__name">{name}</h3>
+        <p className="tour__info">{info}</p>
+        <button
+          className="not__interested__btn"
+          onClick={() => handleDeleteTour(id)}
+        >
+          Not interested
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default Tour;
