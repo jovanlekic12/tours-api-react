04804be829714d50c3b49abf9a70a8d85bb7baca
jovanlekic12 <lekicjovan1212@gmail.com>
2024-09-18 15:47:50 +0200

    refactor code and finish project

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,17 @@ import Tour from "./tour";
 function App() {
   const [tours, setTours] = useState([]);
   const [error, setError] = useState(null);
-  const [expand, setExpand] = useState(false);
   const url = "https://www.course-api.com/react-tours-project";
 
   function handleDeleteTour(id) {
     setTours((arr) => arr.filter((tour) => tour.id !== id));
   }
   function limitWords(str, numWords) {
-    // Split the string into an array of words
     const wordsArray = str.split(" ");
 
-    // Slice the array to get the first `numWords` words
     const limitedWordsArray = wordsArray.slice(0, numWords);
 
-    // Join the words back into a string
     return limitedWordsArray.join(" ");
-  }
-  function expandString(string) {
-    return string;
   }
   const fetchData = async () => {
     try {
@@ -41,12 +34,10 @@ function App() {
     fetchData();
   }, []);
 
-  // if (tours.length === 0) return <h1>Loading...</h1>;
-
   return (
     <div className="container">
       <h1>Our Tours</h1>
-      {/* {error && <p>Error: {error.message}</p>} */}
+      {error && <p>Error: {error.message}</p>}
       <ul className="tours__list">
         {tours.length == 0 ? (
           <button onClick={handleRefresh} className="refresh__btn">
